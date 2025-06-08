eee39ea0b4d9c6d8e4813d0284902aba3618639e
ShambaC <38806897+ShambaC@users.noreply.github.com>
2025-06-08 11:42:43 +0530

    fix: reverse geocoding blank issue

--- Dataset Tools/copernicus_api/utils/rev_geocode.py
+++ Dataset Tools/copernicus_api/utils/rev_geocode.py
@@ -17,8 +17,17 @@ def get_country(lat: float, lon: float) -> str :
 
     response = requests.get(api_url, headers={'X-Api-Key': os.environ[random.choice(API_KEY_LIST)]})
     if response.status_code == requests.codes.ok:
-        x = json.loads(response.text[1:-1])
-        return x["country"]
+        try:
+            if response.text == "[]" :
+                return "error"
+            
+            x = json.loads(response.text[1:-1])
+            return x["country"]
+        except :
+            print(f"Response: {response.text}")
+            raise Exception("API Ninja error occured")
+
+        
     else:
         print("Error:", response.status_code, response.text)
         return "error"
