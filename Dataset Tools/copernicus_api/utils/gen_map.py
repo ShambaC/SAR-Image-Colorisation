22fef3f629af0de5bab4d2f7fc95ff1c836e874b
ShambaC <38806897+ShambaC@users.noreply.github.com>
2025-06-09 10:08:48 +0530

    chore: update map gen

--- Dataset Tools/copernicus_api/utils/gen_map.py
+++ Dataset Tools/copernicus_api/utils/gen_map.py
@@ -1,7 +1,27 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
-df = pd.read_csv("../data/global_points.csv") 
+import os
+
+total_list = os.listdir(f"../../../Dataset")
+csv_list = [x for x in total_list if x.endswith(".csv")]
+
+lon_list = []
+lat_list = []
+
+for csv_file in tqdm(csv_list) :
+    df = pd.read_csv(f"../../../Dataset/{csv_file}")
+
+    if df.shape[0] == 0 :
+        continue
+
+    coords = df.iloc[0]["coordinates"][1:-1].split(",")
+    lon = float(coords[0])
+    lat = float(coords[1])
+
+    lon_list.append(lon)
+    lat_list.append(lat)
 
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
@@ -9,6 +29,6 @@ plt.figure(figsize=(12, 6))
 ax = plt.axes(projection=ccrs.PlateCarree())
 ax.coastlines()
 ax.add_feature(cfeature.BORDERS)
-ax.scatter(df["Longitude"], df["Latitude"], s=10, c='red', alpha=0.7, transform=ccrs.PlateCarree())
+ax.scatter(lon_list, lat_list, s=10, c='red', alpha=0.7, transform=ccrs.PlateCarree())
 plt.title("Points on World Map")
 plt.show()
