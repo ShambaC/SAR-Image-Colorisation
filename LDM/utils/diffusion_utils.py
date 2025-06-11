0d7c14d7b2ebddc52e56f2e4a3f343358c11bac2
ShambaC <38806897+ShambaC@users.noreply.github.com>
2025-06-11 09:24:13 +0530

    fix: latent loading

--- LDM/utils/diffusion_utils.py
+++ LDM/utils/diffusion_utils.py
@@ -11,7 +11,7 @@ def load_latents(latent_path):
     :return:
     """
     latent_maps = {}
-    for fname in glob.glob(os.path.join(latent_path, '*.pkl')):
+    for fname in glob.glob(os.path.join(latent_path, '*.pt')):
         s = pickle.load(open(fname, 'rb'))
         for k, v in s.items():
             latent_maps[k] = v[0]
